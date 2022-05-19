0b0acfc91f913184b2938c480565bc8655c2a0be
Enrique Delgado <enrique.delgado@dreambox.com>
2022-05-18 23:23:30 -0500

    login basic components added

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-
-
+import { MaterialModule } from '../shared/material/material.module';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule
+  ],
 })
 export class LoginModule { }
