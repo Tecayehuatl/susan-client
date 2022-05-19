201b4f4a90b626d43fbd6c10375ea16d9622c636
Enrique Delgado <enrique.delgado@dreambox.com>
2022-05-18 23:23:30 -0500

    login basic components added

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MatFormFieldModule,
     MatInputModule,
     MatButtonModule
   ],
   exports: [
+    MatFormFieldModule,
     MatInputModule,
     MatButtonModule
   ]
