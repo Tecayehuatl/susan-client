047b06974462629eab03401ac4f921c360c38fa2
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-10 13:36:17 -0600

    folders and files removed since we are starting from scratch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
-import { HomeModule } from './home/home.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-
 
 @NgModule({
   declarations: [
@@ -17,8 +14,6 @@ import { DashboardModule } from './dashboard/dashboard.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HomeModule,
-    DashboardModule,
     AppRoutingModule
   ],
   providers: [],
