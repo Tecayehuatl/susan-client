ca9173caedbd347597742504cdc40ef5a1b44bc2
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-10 12:53:36 -0600

    upgrade angular material to v15

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
+import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
+import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
 
 @NgModule({
   declarations: [],
