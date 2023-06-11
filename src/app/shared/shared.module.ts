aca120f6521e2130a1a0d088c28e13c433826cc5
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-11 17:10:04 -0600

    modules reorganized

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
-import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 
-
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    RouterModule
-  ],
-  exports:[
-    MaterialModule,
-    HeaderComponent
-  ]
+    declarations: [],
+    imports: [],
+    exports: [CommonModule, MaterialModule, RouterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
