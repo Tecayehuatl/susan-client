96a50390c4aba2922cd8a9f33ddc15704078fdc1
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-11 17:04:23 -0600

    Patients empty component added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { PatientsComponent } from './patients/patients.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [PatientsComponent],
     imports: [CommonModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
