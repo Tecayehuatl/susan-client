48dd341d0cb01e0b24fc84d04a223d40db394694
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-12 11:26:40 -0600

    create edit patients modal added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { PatientsComponent } from './patients/patients.component';
 import { SharedModule } from '../shared/shared.module';
+import { CreateEditPatientComponent } from './patients/create-edit-patient/create-edit-patient.component';
 
 @NgModule({
-    declarations: [PatientsComponent],
+    declarations: [PatientsComponent, CreateEditPatientComponent],
     imports: [DashboardRoutingModule, SharedModule],
 })
 export class DashboardModule {}
---
+++ src/app/dashboard/patients/create-edit-patient/create-edit-patient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-patient',
+  templateUrl: './create-edit-patient.component.html',
+  styleUrls: ['./create-edit-patient.component.scss']
+})
+export class CreateEditPatientComponent {
+
+}
