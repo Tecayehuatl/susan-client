764a74b57175a9bb59f375b7e17234853970f8b8
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-11 20:06:15 -0600

    router improved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [
-    {
-        path: 'dashboard',
-        component: LayoutComponent,
-    },
-    // {
-    //     path: 'login',
-    //     component: LayoutComponent,
-    // },
-];
+const routes: Routes = [];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
