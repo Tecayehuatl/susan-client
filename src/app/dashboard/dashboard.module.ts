61b7543fb2f25fd0ef276a8cca38c65d824f47ea
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-13 09:48:43 -0600

    Orders componennt added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,12 +5,14 @@ import { PatientsComponent } from './patients/patients.component';
 import { SharedModule } from '../shared/shared.module';
 import { CreateEditPatientComponent } from './patients/create-edit-patient/create-edit-patient.component';
 import { PatientDetailComponent } from './patients/patient-detail/patient-detail.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
     declarations: [
-        PatientsComponent,
         CreateEditPatientComponent,
+        PatientsComponent,
         PatientDetailComponent,
+        OrdersComponent,
     ],
     imports: [DashboardRoutingModule, SharedModule],
 })
