5c6d47eec1fadf3ac3bc90cf2d16c9369cf61f1a
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-16 11:57:48 -0600

    mask input added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,19 @@ import { MaterialModule } from './material/material.module';
 import { RouterModule } from '@angular/router';
 import { GenericModalComponent } from './components/generic-modal/generic-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 const imports = [GenericModalComponent];
 
 @NgModule({
     declarations: [...imports],
-    imports: [MaterialModule],
-    exports: [CommonModule, MaterialModule, RouterModule, ReactiveFormsModule],
+    imports: [MaterialModule, NgxMaskModule.forRoot()],
+    exports: [
+        CommonModule,
+        MaterialModule,
+        RouterModule,
+        ReactiveFormsModule,
+        NgxMaskModule,
+    ],
 })
 export class SharedModule {}
