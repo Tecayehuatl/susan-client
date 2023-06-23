bbd3c66624495750d0ac0c87bbbd879d79029cc5
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-22 18:31:44 -0600

    fix variable name

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,8 +1,8 @@
 // This will return the grandTotal, with or without discount
 export function getGrandTotal(price: number, porcentage: number): number {
     if (porcentage) {
-        const grandTotal = price * (+porcentage / 100);
-        return price - +grandTotal.toFixed(2);
+        const discount = price * (+porcentage / 100);
+        return price - +discount.toFixed(2);
     } else {
         return price;
     }
