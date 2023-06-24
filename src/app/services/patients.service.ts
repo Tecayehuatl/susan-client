e4eeb59398ab69d2ffdc83c4a47c02ea8b7d261b
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-23 20:54:25 -0600

    patient service methods added

--- src/app/services/patients.service.ts
+++ src/app/services/patients.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Patient } from '../dashboard/patients/patients.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class PatientsService {
+    constructor(private http: HttpClient) {}
 
-  constructor() { }
+    getPatients(): Observable<Patient[]> {
+        return this.http.get<Patient[]>(`${environment.baseUrl}/patients`);
+    }
+
+    createPatient(patient: Patient): Observable<Patient> {
+        return this.http.post<Patient>(
+            `${environment.baseUrl}/patients`,
+            patient
+        );
+    }
+
+    updatePatient(patient: Patient): Observable<Patient> {
+        return this.http.put<Patient>(
+            `${environment.baseUrl}/patients/${patient.patient_id}`,
+            patient
+        );
+    }
+
+    deletePatient(id: string) {
+        return this.http.delete(`${environment.baseUrl}/patients/${id}`);
+    }
 }
