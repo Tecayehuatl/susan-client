707084ac18629b7d4e98a5d3f064558f054d2882
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-28 21:00:07 -0600

    not found component added to references but no included in path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
     },
+    // {
+    //     path: '**',
+    //     component: NotFoundComponent,
+    // },
 ];
 
 @NgModule({
