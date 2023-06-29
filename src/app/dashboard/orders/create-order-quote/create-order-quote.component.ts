f57a2f6390cc76169321b035af3109f03d03d4e8
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-28 21:41:29 -0600

    create order component initialized

--- src/app/dashboard/orders/create-order-quote/create-order-quote.component.ts
+++ src/app/dashboard/orders/create-order-quote/create-order-quote.component.ts
@@ -1,9 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { StudiesService } from 'src/app/services/studies.service';
 import { Study } from '../../studies/studies.component';
 import { OrdersQuotesService } from 'src/app/services/orders-quotes.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
     selector: 'app-create-order-quote',
@@ -13,8 +15,28 @@ import { OrdersQuotesService } from 'src/app/services/orders-quotes.service';
 export class CreateOrderQuoteComponent implements OnInit {
     mode!: 'create' | 'edit';
     title!: string;
+    displayedColumns: string[] = [
+        'study_id',
+        'name',
+        'alias',
+        'price',
+        'discountPercentage',
+        'grandTotal',
+        'deliveryDays',
+        'delete',
+    ];
+    dataSource!: MatTableDataSource<Study>;
+
     orderForm!: FormGroup;
     studies: Study[] = [];
+    firstFormGroup = this.fb.group({
+        firstCtrl: ['', Validators.required],
+    });
+    secondFormGroup = this.fb.group({
+        secondCtrl: ['', Validators.required],
+    });
+    isLinear = false;
+    @ViewChild(MatSort) sort!: MatSort;
 
     constructor(
         public dialogRef: MatDialogRef<CreateOrderQuoteComponent>,
@@ -29,6 +51,20 @@ export class CreateOrderQuoteComponent implements OnInit {
             .getStudies()
             .subscribe((studies: Study[]) => (this.studies = studies));
         this.createForm();
+        const temp: Study[] = [
+            {
+                study_id: '1',
+                name: 'EGO TEMP',
+                alias: '',
+                price: 10,
+                discountPercentage: 0,
+                grandTotal: 90,
+                deliveryDays: 1,
+                conditions: '',
+                notes: '',
+            },
+        ];
+        this.dataSource = new MatTableDataSource(temp as Study[]);
     }
 
     createForm(): void {
@@ -55,6 +91,24 @@ export class CreateOrderQuoteComponent implements OnInit {
                     formValues: response,
                 });
             });
+    }
+
+    applyFilter(event: Event): void {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+
+        if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+        }
+    }
+
+    removeAt(index: number): void {
+        const data = this.dataSource.data;
+        // data.splice(
+        //     this.paginator.pageIndex * this.paginator.pageSize + index,
+        //     1
+        // );
+        this.dataSource.data = data;
     }
 }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,12 +6,20 @@ import { GenericModalComponent } from './components/generic-modal/generic-modal.
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
 import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DiscountsComponent } from './components/discounts/discounts.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
-const imports = [GenericModalComponent, OrderSummaryComponent];
+const imports = [
+    GenericModalComponent,
+    OrderSummaryComponent,
+    DiscountsComponent,
+    PaymentComponent,
+];
 
 @NgModule({
-    declarations: [...imports],
-    imports: [MaterialModule, NgxMaskModule.forRoot()],
+    declarations: [...imports, NotFoundComponent],
+    imports: [CommonModule, MaterialModule, NgxMaskModule.forRoot()],
     exports: [
         ...imports,
         CommonModule,
