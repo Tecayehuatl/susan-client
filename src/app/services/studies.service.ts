ae4653877181208f0ad266e4ca9322045e1a6ba3
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-06-30 10:22:07 -0600

    search method for studies added

--- src/app/services/studies.service.ts
+++ src/app/services/studies.service.ts
@@ -14,6 +14,14 @@ export class StudiesService {
         return this.http.get<Study[]>(`${environment.baseUrl}/studies`);
     }
 
+    searchStudies(query: string): Observable<Study[]> {
+        return this.http.get<Study[]>(`${environment.baseUrl}/studies/search`, {
+            params: {
+                q: query,
+            },
+        });
+    }
+
     createStudy(study: Study): Observable<Study> {
         return this.http.post<Study>(`${environment.baseUrl}/studies`, study);
     }
