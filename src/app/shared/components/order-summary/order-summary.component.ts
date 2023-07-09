c16d9010ae89a36659196e0076d3f97a1261dbc8
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-07-09 01:15:18 -0600

    order prices updated

--- src/app/shared/components/order-summary/order-summary.component.ts
+++ src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,8 +1,79 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { FormArray } from '@angular/forms';
+import { OperationType, transformPrices } from '../../utils/utils';
+import { Study } from 'src/app/dashboard/studies/studies.component';
+import { Discount } from 'src/app/services/discounts.service';
 
 @Component({
     selector: 'app-order-summary',
     templateUrl: './order-summary.component.html',
     styleUrls: ['./order-summary.component.scss'],
 })
-export class OrderSummaryComponent {}
+export class OrderSummaryComponent implements AfterViewInit {
+    @Input() studiesForm!: FormArray;
+    @Input() discountsForm!: FormArray;
+    total = 0;
+    subtotal = 0;
+    discount = 0;
+    tax = 0;
+    grandTotal = 0;
+    studies: any;
+    discounts!: any[];
+
+    ngAfterViewInit(): void {
+        this.studiesForm.valueChanges.subscribe((studies) => {
+            this.studies = studies;
+            this.calculateOrder(
+                this.studies,
+                this.extractDiscounts(this.discounts)
+            );
+        });
+        this.discountsForm.valueChanges.subscribe((discounts) => {
+            console.log('discounts ', discounts);
+            this.discounts = discounts;
+            this.calculateOrder(
+                this.studies,
+                this.extractDiscounts(this.discounts)
+            );
+        });
+    }
+
+    calculateOrder(studies: Study[], discounts: any[]): void {
+        this.total = transformPrices(
+            studies,
+            discounts,
+            OperationType.TOTAL_CALCULATION
+        );
+        this.subtotal = transformPrices(
+            studies,
+            discounts,
+            OperationType.SUBTOTAL_CALCULATION
+        );
+        this.discount = transformPrices(
+            studies,
+            discounts,
+            OperationType.DISCOUNT_CALCULATION
+        );
+        this.tax = transformPrices(
+            studies,
+            discounts,
+            OperationType.TAX_CALCULATION
+        );
+        this.grandTotal = transformPrices(
+            studies,
+            discounts,
+            OperationType.GRAND_TOTAL_CALCULATION
+        );
+    }
+
+    extractDiscounts(discountsObject: any[]): number[] {
+        let discounts: any[] = [];
+        if (discountsObject?.length > 0) {
+            discountsObject.forEach((discount: Discount) => {
+                if (discount.discountPercentage)
+                    discounts = [...discounts, discount.discountPercentage];
+            });
+        }
+        return discounts;
+    }
+}
--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,3 +1,5 @@
+import { Study } from 'src/app/dashboard/studies/studies.component';
+
 // This will return the grandTotal, with or without discount
 export function getGrandTotal(price: number, porcentage: number): number {
     if (porcentage) {
@@ -5,5 +7,78 @@ export function getGrandTotal(price: number, porcentage: number): number {
         return price - +discount.toFixed(2);
     } else {
         return price;
+    }
+}
+
+export enum OperationType {
+    TOTAL_CALCULATION = 'totalCalculation',
+    SUBTOTAL_CALCULATION = 'subtotalCalculation',
+    DISCOUNT_CALCULATION = 'discountCalculation',
+    TAX_CALCULATION = 'taxCalculation',
+    GRAND_TOTAL_CALCULATION = 'grandTotalCalculation',
+}
+
+export function transformPrices(_labStudies: Study[], ...args: any[]): number {
+    let total = 0;
+    let subtotal = 0;
+    let discount = 0;
+    let tax = 0;
+    let grandTotal = 0;
+
+    /**
+     * Dependencies
+     */
+    const TAX_BASE = 16;
+    const _discounts: number[] = args[0] || [];
+    const _operationType: string = args[1] || undefined;
+    let _totalBaseDiscounts = 0;
+
+    /**
+     * 1.- Getting the total
+     */
+    _labStudies.map((study: Study) => {
+        total = total + +study.grandTotal;
+    });
+
+    /**
+     * 2.- Getting the subtotal, NO TAX INCLUDED
+     */
+    subtotal = total / 1.16;
+
+    /**
+     * 3.- This will return values only when discounts are present.
+     *     Getting the discount amount
+     */
+    _totalBaseDiscounts = total;
+    _discounts.map((_discountNumber: number) => {
+        const singleDiscount = _totalBaseDiscounts * (_discountNumber / 100);
+        _totalBaseDiscounts = _totalBaseDiscounts - singleDiscount;
+
+        discount = discount + singleDiscount;
+    });
+
+    /**
+     * 4.- Getting the grand total(final payment)
+     */
+    grandTotal = total - discount;
+
+    /**
+     * 5.- Getting the tax from grand total(last step)
+     */
+    tax = grandTotal - grandTotal / 1.16;
+
+    switch (_operationType) {
+        case OperationType.TOTAL_CALCULATION:
+            return total;
+        case OperationType.SUBTOTAL_CALCULATION:
+            return subtotal;
+        case OperationType.DISCOUNT_CALCULATION:
+            return discount;
+        case OperationType.TAX_CALCULATION:
+            return tax;
+        case OperationType.GRAND_TOTAL_CALCULATION:
+            return grandTotal;
+        default:
+            return 0;
     }
 }
