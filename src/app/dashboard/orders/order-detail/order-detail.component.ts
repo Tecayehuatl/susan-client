85c09599a1ac8b51292ab997e303bb23cc3dc8c0
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-08-09 17:42:23 -0600

    replacing harcoded data

--- src/app/dashboard/orders/order-detail/order-detail.component.ts
+++ src/app/dashboard/orders/order-detail/order-detail.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
+import { Order } from 'src/app/services/orders-quotes.service';
 
 @Component({
     selector: 'app-order-detail',
@@ -13,70 +14,31 @@ export class OrderDetailComponent implements AfterViewInit {
     orderId!: string;
     displayedColumns: string[] = [
         'id',
-        'creationDate',
         'studyName',
         'alias',
+        'deliveryDays',
         'price',
         'discount',
+        'grandTotal',
     ];
     dataSource!: MatTableDataSource<any>;
+    orderDetail: Order;
 
     @ViewChild(MatPaginator) paginator!: MatPaginator;
     @ViewChild(MatSort) sort!: MatSort;
 
     constructor(private route: ActivatedRoute) {
         this.orderId = this.route.snapshot.params['patientId'];
-        // Create 100 users
-        const users = Array.from({ length: 100 }, (_, k) =>
-            createNewUser(k + 1)
-        );
 
         // Assign the data to the data source for the table to render
-        this.dataSource = new MatTableDataSource(users);
+        this.orderDetail = this.route.snapshot.data['orderDetail'];
+        this.dataSource = new MatTableDataSource(
+            this.orderDetail.order_studies
+        );
     }
 
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
     }
-}
-
-const NAMES: string[] = [
-    'Maia',
-    'Asher',
-    'Olivia',
-    'Atticus',
-    'Amelia',
-    'Jack',
-    'Charlotte',
-    'Theodore',
-    'Isla',
-    'Oliver',
-    'Isabella',
-    'Jasper',
-    'Cora',
-    'Levi',
-    'Violet',
-    'Arthur',
-    'Mia',
-    'Thomas',
-    'Elizabeth',
-];
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): any {
-    const name =
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-        ' ' +
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-        '.';
-
-    return {
-        id: id.toString(),
-        creationDate: 'Sep 11 2023',
-        studyName: 'Biometría Hemática',
-        alias: 'BH',
-        price: '$100.00',
-        discount: '$10.00',
-    };
 }
