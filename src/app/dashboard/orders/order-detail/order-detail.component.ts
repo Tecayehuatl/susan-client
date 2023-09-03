b12d734b2de40ec3e5ccfc3d18a013958a1dbd8a
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-09-03 16:55:26 -0600

    mechanism to render dynamic components added

--- src/app/dashboard/orders/order-detail/order-detail.component.ts
+++ src/app/dashboard/orders/order-detail/order-detail.component.ts
@@ -1,16 +1,27 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ComponentRef,
+    OnInit,
+    ViewChild,
+    ViewContainerRef,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSidenav } from '@angular/material/sidenav';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { Order } from 'src/app/services/orders-quotes.service';
+import { NotesComponent } from 'src/app/shared/components/notes/notes.component';
+import { PaymentHistoricalTransactionsComponent } from 'src/app/shared/components/payment-historical-transactions/payment-historical-transactions.component';
+import { AdHostDirective } from 'src/app/shared/directives/ad-host.directive';
 
 @Component({
     selector: 'app-order-detail',
     templateUrl: './order-detail.component.html',
     styleUrls: ['./order-detail.component.scss'],
 })
-export class OrderDetailComponent implements AfterViewInit {
+export class OrderDetailComponent implements OnInit, AfterViewInit {
     orderId!: string;
     displayedColumns: string[] = [
         'id',
@@ -23,7 +34,11 @@ export class OrderDetailComponent implements AfterViewInit {
     ];
     dataSource!: MatTableDataSource<any>;
     orderDetail: Order;
+    isLoading = true;
+    viewContainerRef!: ViewContainerRef;
 
+    @ViewChild(AdHostDirective, { static: true }) adHost!: AdHostDirective;
+    @ViewChild(MatSidenav) sidenav!: MatSidenav;
     @ViewChild(MatPaginator) paginator!: MatPaginator;
     @ViewChild(MatSort) sort!: MatSort;
 
@@ -37,8 +52,58 @@ export class OrderDetailComponent implements AfterViewInit {
         );
     }
 
+    ngOnInit(): void {}
+
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
+    }
+
+    renderDynamicComponent(type: string): void {
+        this.viewContainerRef = this.adHost.viewContainerRef;
+        this.viewContainerRef.clear();
+        this.sidenav?.open().then(() => {
+            this.isLoading = true;
+            let componentInstance: ComponentRef<any>;
+
+            switch (type) {
+                case 'historicPayment':
+                    console.log('Creating hisoric payment');
+                    componentInstance = this.viewContainerRef.createComponent(
+                        PaymentHistoricalTransactionsComponent
+                    );
+                    componentInstance.instance.title =
+                        'Historial de pagos realizados';
+                    this.isLoading = false;
+                    componentInstance.onDestroy(() => {
+                        this.isLoading = false;
+                        this.viewContainerRef.clear();
+                    });
+
+                    break;
+                case 'notes':
+                    console.log('Creating notes');
+                    componentInstance =
+                        this.viewContainerRef.createComponent(NotesComponent);
+                    componentInstance.instance.title = 'Notas';
+                    this.isLoading = false;
+                    componentInstance.onDestroy(() => {
+                        this.isLoading = false;
+                        this.viewContainerRef.clear();
+                    });
+
+                    break;
+                default:
+                    this.isLoading = false;
+                    this.viewContainerRef.clear();
+                    this.sidenav.close();
+                    break;
+            }
+        });
+    }
+
+    closeSidenav(): void {
+        this.sidenav.close();
+        this.viewContainerRef.clear();
     }
 }
