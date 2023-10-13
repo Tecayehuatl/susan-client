053d9b6dbe3e1710a2e603bfae8f8eefc7647e11
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-10-13 11:15:50 -0600

    sorting event added to sort in asc or desc way the orders; app-label component was added to improve the UI for different status and items per page size changed

--- src/app/dashboard/orders/orders.component.ts
+++ src/app/dashboard/orders/orders.component.ts
@@ -1,22 +1,52 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Patient } from '../patients/patients.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GenericModalComponent } from 'src/app/shared/components/generic-modal/generic-modal.component';
+import {
+    Order,
+    OrdersQuotesService,
+} from 'src/app/services/orders-quotes.service';
 
 @Component({
     selector: 'app-orders',
     templateUrl: './orders.component.html',
     styleUrls: ['./orders.component.scss'],
 })
-export class OrdersComponent implements OnInit {
-    @Input() title!: string;
-    @Input() ordersQuotes!: any[];
+export class OrdersComponent {
+    sortedOrdersQuotes!: Order[];
+
     @ViewChild(MatPaginator) paginator!: MatPaginator;
     @ViewChild(MatSort) sort!: MatSort;
+    @Input() title!: string;
+    @Input() set incomingOrdersQuotes(ordersQuotes: Order[]) {
+        // Initial sort and resort every time that this input changes
+        this.sortedOrdersQuotes = this.sortData(
+            {
+                active: 'creationDate',
+                direction: 'desc',
+            } as Sort,
+            ordersQuotes
+        );
+        this.dataSource.data = this.sortedOrdersQuotes;
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+    }
+    @Output() setNewincomingOrdersQuotes: EventEmitter<Order[]> =
+        new EventEmitter<Order[]>();
 
-    dataSource!: MatTableDataSource<any>;
+    dataSource: MatTableDataSource<any> = new MatTableDataSource(
+        this.sortedOrdersQuotes
+    );
     displayedColumns: string[] = [
         'id',
         'creationDate',
@@ -24,20 +54,112 @@ export class OrdersComponent implements OnInit {
         'paymentStatus',
         'deliverStatus',
         'grandTotal',
+        // TODO: remove balance and make it dinamyc when is a quote
+        'balance',
+        'delete',
     ];
 
-    constructor(private route: ActivatedRoute, private router: Router) {}
+    constructor(
+        private _snackBar: MatSnackBar,
+        private dialog: MatDialog,
+        private orderQuoteService: OrdersQuotesService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {}
 
-    ngOnInit(): void {
-        this.dataSource = new MatTableDataSource(this.ordersQuotes);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-    }
-
-    navigateToOrderQuote(itemData?: any): void {
+    navigateToOrderQuote(order: Order): void {
         const patiendId = this.route.snapshot.params['patientId'];
         this.router.navigateByUrl(
-            `/dashboard/patients/${patiendId}/${itemData?.order_id}`
+            `/dashboard/patients/${patiendId}/${order?.order_id}`
         );
+    }
+
+    openDeleteDialog(orderId: string, event: Event, index: number): void {
+        event.stopPropagation();
+
+        const dialogRef = this.dialog.open(GenericModalComponent, {
+            minWidth: '800px',
+            data: {
+                title: `¿SEGURO QUE DESEA ELIMINAR LA ORDEN"?`,
+                actions: {
+                    main: 'ELIMINAR ORDEN',
+                    secondary: 'CANCELAR',
+                },
+            },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result === true) {
+                this.orderQuoteService.deleteOrder(orderId).subscribe(() => {
+                    this.removeAt(index);
+                    this._snackBar.open(`ORDEN BORRADA`, 'CERRAR');
+                });
+            }
+        });
+    }
+
+    removeAt(index: number): void {
+        const data = this.dataSource.data;
+        data.splice(
+            this.paginator.pageIndex * this.paginator.pageSize + index,
+            1
+        );
+        this.dataSource.data = data;
+        // Notifying to the parent that one item has been removed
+        this.setNewincomingOrdersQuotes.emit(data);
+    }
+
+    sortDataEvent(sort: Sort): void {
+        this.dataSource.data = this.sortData(sort, this.sortedOrdersQuotes);
+    }
+
+    sortData(sort: Sort, orderQuotes: Order[]): Order[] {
+        let ordersSorted: Order[] = [];
+
+        const data = orderQuotes.slice();
+        if (!sort.active || sort.direction === '') {
+            ordersSorted = data;
+            return ordersSorted;
+        }
+
+        ordersSorted = data.sort((a: any, b: any) => {
+            const isAsc = sort.direction === 'asc';
+            switch (sort.active) {
+                case 'id':
+                    return this.compare(a.order_id, b.order_id, isAsc);
+                case 'creationDate':
+                    return this.compare(a.created_at, b.created_at, isAsc);
+                case 'generalStatus':
+                    return this.compare(
+                        a.order_status_id,
+                        b.order_status_id,
+                        isAsc
+                    );
+                case 'paymentStatus':
+                    return this.compare(
+                        a.payment_status_id,
+                        b.payment_status_id,
+                        isAsc
+                    );
+                case 'deliverStatus':
+                    return this.compare(
+                        a.delivery_status_id,
+                        b.delivery_status_id,
+                        isAsc
+                    );
+                case 'grandTotal':
+                    return this.compare(a.grand_total, b.grand_total, isAsc);
+                case 'balance':
+                    return this.compare(a.balance, b.balance, isAsc);
+                default:
+                    return 0;
+            }
+        });
+
+        return ordersSorted;
+    }
+
+    compare(a: number | string, b: number | string, isAsc: boolean) {
+        return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
     }
 }
