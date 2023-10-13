0178ef92c9410b55cad01cec50aafb237f0cf592
Enrique Delgado Tecayehuatl <enrique.tecayehuatl@meltwater.com>
2023-10-13 12:26:00 -0600

    delete event added and styles were added to the historic info component

--- src/app/shared/components/historic-info/historic-info.component.ts
+++ src/app/shared/components/historic-info/historic-info.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { GenericModalComponent } from '../generic-modal/generic-modal.component';
 
 @Component({
     selector: 'app-historic-info',
@@ -7,11 +9,33 @@ import { Component, Input } from '@angular/core';
 })
 export class HistoricInfoComponent {
     @Input() historicInfoItem!: HistoricInfoItem;
+    @Output() deleteEmmiter: EventEmitter<boolean> = new EventEmitter();
+
+    constructor(private dialog: MatDialog) {}
+
+    openDeleteModal(): void {
+        const dialogRef = this.dialog.open(GenericModalComponent, {
+            minWidth: '800px',
+            data: {
+                title: `¿SEGURO QUE DESEA ELIMINAR El ITEM"?`,
+                actions: {
+                    main: 'ELIMINAR',
+                    secondary: 'CANCELAR',
+                },
+            },
+        });
+
+        dialogRef.afterClosed().subscribe((response) => {
+            if (response === true) this.deleteEmmiter.emit();
+        });
+    }
 }
 
 export interface HistoricInfoItem {
+    id?: string;
     title?: string;
     subtitle?: string;
     description?: string;
     date?: string;
+    isDeleteActionIncluded?: boolean;
 }
