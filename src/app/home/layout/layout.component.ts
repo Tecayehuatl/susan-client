3884e29008e5a6ebf6f1b400c57cc1b5682f2899
Enrique Delgado Tecayehuatl <enrique.delgado@live.com>
2024-07-07 02:53:13 -0600

    feature(Home): Moving the html code to his own component

--- src/app/home/layout/layout.component.ts
+++ src/app/home/layout/layout.component.ts
@@ -23,19 +23,6 @@ export class LayoutComponent {
         },
     ];
 
-    public descriptions = [
-        {
-            title: 'Misión',
-            description:
-                'Brindar a nuestros usuarios servicios de análisis clínicos de calidad, confiables, eficientes y oportunos que ayuden a prevenir y controlar padecimientos con la ayuda de un equipo humano altamente calificado y con la ayuda de tecnología de vanguardia empleando estándares de calidad.',
-        },
-        {
-            title: 'Visión',
-            description:
-                'Laboratorios Biotecsa busca ser un laboratorio clínico líder reconocido en su región para el 2024, aplicando esquemas de calidad en el servicio, eficiencia y capacitación continua, cumpliendo las leyes, reglas y regulaciones aplicables además de ofrecer tecnología de vanguardia para otorgar resultados confiables.',
-        },
-    ];
-
     public footerMenu = [
         {
             name: 'Acerca de nosotros',
