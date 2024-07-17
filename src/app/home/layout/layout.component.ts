b21b46e74fd7b1e0ebd92e7a8c5df5fa1e54c6e6
Enrique Delgado Tecayehuatl <enrique.delgado@live.com>
2024-07-17 16:08:35 -0600

    fix(Home & Layout): Menu changed when hover and container for hero was modified and footer changes in menu

--- src/app/home/layout/layout.component.ts
+++ src/app/home/layout/layout.component.ts
@@ -10,35 +10,39 @@ export class LayoutComponent {
         {
             name: 'Acerca de nosotros',
             url: '#about-us',
-            // path:
+            hash: true,
         },
         {
             name: 'Ubicación',
             url: '#locate-me',
-            // path:
+            hash: true,
         },
         {
             name: 'Iniciar sesión',
             url: 'login',
+            hash: false,
         },
     ];
 
     public footerMenu = [
         {
             name: 'Acerca de nosotros',
-            url: '',
+            url: '#about-us',
+            hash: true,
         },
         {
             name: 'Preguntas frecuentes',
-            url: '',
+            url: 'faq',
+            hash: false,
         },
         {
             name: 'Facturación',
-            url: '',
+            url: 'https://wa.me/525517298887?text=%C2%A1Hola%2C%20me%20gustar%C3%ADa%20una%20cotizaci%C3%B3n%21',
+            hash: true,
         },
         {
             name: 'Aviso de privacidad',
-            url: '',
+            url: 'privacy-statement',
         },
     ];
 
