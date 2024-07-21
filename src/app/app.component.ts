69cf6e13b7608159125907a8218426f41ec2188b
Enrique Delgado <enrique.delgado@live.com>
2024-07-21 16:02:35 -0600

    fix(Google analytics): Revert old way to gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { Gtag } from 'angular-gtag';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    constructor(private router: Router, private gtag: Gtag) {}
-
-    ngOnInit() {
-        this.router.events.subscribe((event) => {
-            if (event instanceof NavigationEnd) {
-                this.gtag.pageview({
-                    page_path: event.urlAfterRedirects,
-                });
-            }
-        });
-    }
+export class AppComponent {
+    constructor() {}
 }
