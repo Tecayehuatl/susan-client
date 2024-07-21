a4809a1c60988cbfc595a34debdc032dc6c4cd55
Enrique Delgado <enrique.delgado@live.com>
2024-07-21 11:11:39 -0600

    feature(Gtag & SP): Security policy added and google analytics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Gtag } from 'angular-gtag';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    constructor(gtag: Gtag) {}
+}
